1f73cca33ed1fcca5144e21125b45e01992671ff
polarune(desktop) <jumpcross123@naver.com>
2026-05-10 11:04:29 +0900

    프로젝트 생성 및 배경화면 추가

---
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myandroidproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myandroidproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+//        // 디버그 빌드일 때에는 1초 후 게임 화면으로 바로 넘어가게 한다
+//        if (BuildConfig.DEBUG) {
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                startGameActivity()
+//            }, 1000)
+//        }
+    }
+
+    fun onBtnStartGame(view: View) {
+        startGameActivity()
+    }
+
+    private fun startGameActivity() {
+        val intent = Intent(this, SuikaGameActivity::class.java)
+        startActivity(intent)
+    }
+
+}
